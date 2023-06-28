a80755f826a5075fbf5df84c9aef747237186de5
Samael Szep <szepsamael@gmail.com>
2023-06-28 17:03:28 +0200

    dynamic routes added

---
+++ Complete-challenges/NEXT/lotr-app/pages/volumes/[slug].js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { volumes } from "../../lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const volume = volumes.find((volume) => volume.slug === slug);
+  const volumeIndex = volumes.findIndex((volume) => volume.slug === slug);
+  const previousVolumeSlug =
+    volumeIndex > 0 ? volumes[volumeIndex - 1].slug : null;
+  const nextVolumeSlug =
+    volumeIndex < volumes.length - 1 ? volumes[volumeIndex + 1].slug : null;
+
+  return (
+    <>
+      {" "}
+      <div key={volume.slug}>
+        <h2>{volume.title}</h2>
+        <p>{volume.description}</p>
+        <Image src={volume.cover} alt={volume.title} width={144} height={144} />
+        {volume.books.map((book) => (
+          <div key={book.ordinal}>
+            <h3>
+              {book.ordinal}
+              {":  "}
+              {book.title}
+            </h3>
+          </div>
+        ))}
+      </div>
+      <Link href="../">Home</Link>
+      <br />
+      <Link href="/volumes">← All Volumes</Link>
+      <br />
+      {previousVolumeSlug && (
+        <Link href={`/volumes/${previousVolumeSlug}`}>Previous Volume</Link>
+      )}
+      <br />
+      {nextVolumeSlug && (
+        <Link href={`/volumes/${nextVolumeSlug}`}>Next Volume</Link>
+      )}
+    </>
+  );
+}
--- Complete-challenges/NEXT/lotr-app/pages/volumes/index.js
+++ Complete-challenges/NEXT/lotr-app/pages/volumes/index.js
@@ -1,27 +1,32 @@
-import { introduction } from "../../lib/data";
+import { useRouter } from "next/router";
+import { introduction, volumes } from "../../lib/data";
 import Link from "next/link";
 
 export default function Books() {
+  const router = useRouter();
+  const handleRandomVolume = () => {
+    const randomVolume = getRandomElement(volumes);
+    router.push(`/volumes/${randomVolume.slug}`);
+  };
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   return (
     <>
       <h1>Lord of the Rings</h1>
       <p>{introduction}</p>
       <h2>All volumes</h2>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The fellowship of the ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The two towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The return of the king
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button onClick={handleRandomVolume}>Random Volume</button>
+      <br />
+      <Link href="../">Home</Link>
     </>
   );
 }
